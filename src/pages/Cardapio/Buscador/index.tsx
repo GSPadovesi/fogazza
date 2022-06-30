a79777c3383271ab5b260b9d840cd93f6de8bb6d
Gabriel Santana <gabriel@grupoeureka.com.br>
2022-06-30 19:06:22 -0300

    Aula 04

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -12,7 +12,8 @@ export default function Buscador({busca, setBusca} : props){
         <input 
             value={busca}
             onChange={evento => setBusca(evento.target.value)}
-        />
+            placeholder="Buscar"
+       />
         <CgSearch
             size={20}
             color="#4C4D5E"
